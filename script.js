e0b20b457dea2ab20eab53e7327edfa34b0ffbaf
PriRum <prirumel@gmail.com>
2026-09-08 23:52:10 -0300

    main code

---
+++ script.js
@@ -0,0 +1,10 @@
+/*animaciones y toggles del sidebar*/
+const menu = document.getElementById('menu');
+const sidebar = document.getElementById('sidebar');
+const main = document.getElementById('main');
+
+menu.addEventListener('click', () => {
+    sidebar.classList.toggle('menu-toggle');
+    menu.classList.toggle('menu-toggle');
+    main.classList.toggle('menu-toggle');
+});
